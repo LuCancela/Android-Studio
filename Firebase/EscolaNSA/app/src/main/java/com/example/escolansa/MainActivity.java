c3263f96dc129d2117cc6368b6f1b7bc4250496b
LuCancela <cancelalucas@gmail.com>
2023-11-13 22:29:16 -0300

    Projeto Escola com Firebase

--- Firebase/EscolaNSA/app/src/main/java/com/example/escolansa/MainActivity.java
+++ Firebase/EscolaNSA/app/src/main/java/com/example/escolansa/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.escolansa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -31,7 +32,38 @@ public class MainActivity extends AppCompatActivity {
         myRef.setValue(comp);
         myRef.setValue(estado);
         myRef.setValue(cep);
+        myRef.setValue(curso);
 
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String codigo = cCodigo.getText().toString();
+                String nome = cNome.getText().toString();
+                String telefone = cTelefone.getText().toString();
+                String email = cEmail.getText().toString();
+                String endereco = cEndereco.getText().toString();
+
+                if (nome.isEmpty()) {
+                    cNome.setError("Este campo é obrigatorio");
+                } else if (codigo.isEmpty()) {
+
+                    db.addPessoa(new Pessoa(nome, telefone, email, endereco));
+                    Toast.makeText(Cadastro.this, "Cadastro salvo com sucesso", Toast.LENGTH_SHORT).show();
+                    listarPessoas();
+                    limpaCampos();
+                    escondeTeclado();
+
+                } else {
+                    db.atualizarPessoa(new Pessoa(Integer.parseInt(codigo), nome, telefone, email, endereco));
+                    Toast.makeText(Cadastro.this, "Cadastro atualizado com sucesso", Toast.LENGTH_SHORT).show();
+                    listarPessoas();
+                    limpaCampos();
+                    escondeTeclado();
+
+                }
+            }
+        });
 
         //DatabaseReference cadastro = CadastroAluno.child("cadastro");
 
@@ -46,9 +78,10 @@ public class MainActivity extends AppCompatActivity {
         tel = findViewById(R.id.tel);
         endereco = findViewById(R.id.endereco);
         comp= findViewById(R.id.comp);
-        estado = findViewById(R.id.estado);
         cidade = findViewById(R.id.cidade);
         cep = findViewById(R.id.cep);
+        curso = findViewById(R.id.curso);
+        estado = findViewById(R.id.estado);
         buttonCadastro = findViewById(R.id.buttonCadastro);
     }
 
